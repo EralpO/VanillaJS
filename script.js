2b94d486ceb4723a74e840e6b15ce33f13cce603
EralpO <eralponay10@gmail.com>
2020-08-06 13:39:33 +0300

    Form Validator update

--- script.js
+++ script.js
@@ -5,10 +5,7 @@ const password2 =document.getElementById('password2')
 const email =document.getElementById('email')
 
 
-function ValidateEmail(email){
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
+
 
 showError = (input,message) =>{  
    const FormControl = input.parentElement;
@@ -23,6 +20,31 @@ showSuccess = (input) =>{
 
 getElementName = (input) =>{
    return input.id.charAt(0).toUpperCase() + input.id.slice(1)
+}
+function checkEmail(email){
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(!re.test(String(email.value).toLowerCase()))
+    showError(email,"Please enter a valid email")
+    else{
+        showSuccess(email)
+    }
+
+}
+
+function checkMatch(password,password2){
+    if(password.value !== password2.value){
+        showError(password2,"Password do not match")
+    }
+    else{
+        showSuccess(password2)
+    }
+}
+function checkLenght(password){
+    if(password.value.trim().length <6)
+    showError(password,"Password atleast need to 6 characters")
+    else{
+        showSuccess(password)
+    }
 }
 
 checkRequired = (inputArr) =>{
@@ -41,6 +63,9 @@ form.addEventListener('submit',function(e){
     e.preventDefault();
    
      checkRequired([email,username,password,password2])
+     checkEmail(email)
+     checkLenght(password)
+     checkMatch(password,password2)
     
 
 });
